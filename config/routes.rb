f29f86ac5577557a371ffe529eff6353f9b2d0fc
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-01 05:05:19 +0000

    管理）商品関連画面のコントローラ作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:new, :index, :show, :edit]
   end
 
   scope module: :public do
