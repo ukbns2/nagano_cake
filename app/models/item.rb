79fc794e2191f2d8a16f593ab739ae85bf9ed919
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-02 00:36:33 +0000

    管理）商品詳細画面の実装完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
+
+  def get_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/nagano_cake/no_image.jpg')
+      image.attach(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/png')
+    end
+    image.variant(resize: size).processed
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :items
+    resources :items, onle: [:new, :create, :index, :show, :edit]
   end
 
   scope module: :public do
