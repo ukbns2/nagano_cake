7b91fb43eea8021e369a3a90470f4926957e26dd
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-09 14:33:22 +0000

    ヘッダーの作成着手

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
+   #顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  #管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
@@ -15,17 +26,7 @@ Rails.application.routes.draw do
     patch 'customers/out' => "customers#out"
   end
 
-  #顧客用
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  #管理者用
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
