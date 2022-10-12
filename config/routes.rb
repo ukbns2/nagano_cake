d03dc8c234981a4efa5a7e971a6ed5d528cd34bd
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-12 11:15:40 +0000

    管理)会員一覧画面のコントローラー作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
    #顧客用
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit]
   end
 
   scope module: :public do
