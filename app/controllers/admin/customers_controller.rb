bd5f9feb97561235e9b725657247a2ec3298d013
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-12 11:58:36 +0000

    管理)管理者のログインアカウントを作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,8 +3,15 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
