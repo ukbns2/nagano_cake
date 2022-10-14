b30ca3ec89f5a8cbbabc8314312fcec7fcfb54e6
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-14 13:15:47 +0000

    管理)会員情報編集画面の実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
-    resources :customers, only: [:index, :show, :edit]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   scope module: :public do
