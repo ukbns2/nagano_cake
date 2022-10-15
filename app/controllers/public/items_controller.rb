b3ee69a4b8097a1ff825df8bb45ba3c0c9649658
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-15 04:35:17 +0000

    顧客)商品一覧ページと詳細画面の実装一部完了(ジャンル検索の表示、カート追加機能未着手)

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
+    @items = Item.page(params[:page]).per(8)
+    @items_all = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
