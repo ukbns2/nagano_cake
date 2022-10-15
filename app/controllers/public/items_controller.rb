1c85c3bc0f0e83f143b5129ec8681d7751ec83fd
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-15 08:02:42 +0000

    顧客)商品関連画面のジャンル検索の表示の実装完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,9 +4,11 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8)
     @items_all = Item.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 end
