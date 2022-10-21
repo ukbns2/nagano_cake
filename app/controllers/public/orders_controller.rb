cdca44499dd6db703597d7c1a23483ac87ea4c93
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-21 14:53:19 +0000

    顧客)ordersのコントローラーのcreateを一部記述

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,32 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def check
+    @order = Order.new(order_params)
+    if params[:order][:address_number] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:address_number] == "1"
+      @order.postal_code = Address.find(params[:order][:address_id]).postal_code
+      @order.address = Address.find(params[:order][:address_id]).address
+      @order.name = Address.find(params[:order][:address_id]).name
+    elsif params[:order][:address_number] == "2"
+      @order = Order.new(order_params)
+      @order.save
+    else
+      redirect_to new_order_path
+    end
+
+  end
+
+  def create
+    order = Order.new(order_params)
+    order.customer_id = current_customer.id
+    order.save
+    redirect_to orders_check_path
+  end
+
   def index
   end
 
@@ -13,4 +39,10 @@ class Public::OrdersController < ApplicationController
 
   def thanks
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
