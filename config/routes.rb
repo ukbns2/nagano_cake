fc187bd57f96b69d000598c2f476225979eb705c
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-24 12:16:56 +0000

    管理)注文詳細画面のコントローラーの作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
   end
 
   scope module: :public do
