6a090c6fb755d1dc5f7bb00a811c1ffdf75671b0
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-26 12:49:27 +0000

    管理)注文履歴詳細画面の実装完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,8 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @total = @order.total_payment - @order.shopping_cost
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show, :update]
+    resources :orders, only: [:show]
   end
 
   scope module: :public do
