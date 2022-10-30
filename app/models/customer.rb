d5fa7d82a17d11e9bd352437804a9baa27c8a554
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-30 07:46:36 +0000

    [更新]顧客)会員ステータスの実装完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,8 @@ class Customer < ApplicationRecord
     super && (is_deleted == false)
   end
 
+  #enum is_deleted: { 有効: 0, 無効: 1 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
