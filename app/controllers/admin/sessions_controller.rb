a0b2a0ceb04ce49c1fbd0c38487961d04fac4f60
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-30 06:08:10 +0000

    [修正]管理)ログイン後の遷移ページの変更

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,4 +24,19 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+   def after_sign_in_path_for(resource)
+     admin_path
+   end
+
+   def after_sign_out_path_for(resource)
+     about_path
+   end
+
+   protected
+
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number])
+   end
+
 end
