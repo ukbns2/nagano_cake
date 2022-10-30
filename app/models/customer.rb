1511a66d2d832ee87fcdd061cd9f2354bcb41f9b
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-30 08:19:09 +0000

    管理)注文履歴一覧画面、履歴詳細画面のレイアウト調整完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,6 @@ class Customer < ApplicationRecord
     super && (is_deleted == false)
   end
 
-  #enum is_deleted: { 有効: 0, 無効: 1 }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
