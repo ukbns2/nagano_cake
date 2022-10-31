6864705a8381f6b6a22eba3b3caf7845b087ca0b
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-10-31 12:41:57 +0000

    [修正・更新]ページの遷移先変更、カートが空の時に次に進めないように設定するなど

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -30,7 +30,7 @@ class Admin::SessionsController < Devise::SessionsController
    end
 
    def after_sign_out_path_for(resource)
-     about_path
+     new_admin_session_path
    end
 
    protected
